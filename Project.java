52d24420e71cbef3dcdf561c0ff5bc91502bd92d
Chris Kang <ckang@fastmail.com>
2018-11-11 17:15:20 -0500

    created main method file

---
+++ Project.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author kangcs
+ *
+ */
+public class Project {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
