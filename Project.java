66833259c0db33119cc176ddaffd78578c566fc8
Chris Kang <ckang@fastmail.com>
2018-11-12 22:27:04 -0500

    Update Project.java

--- Project.java
+++ Project.java
@@ -16,20 +16,29 @@ public class Project {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int customerCount = 0;
+		int contractorCount = 1000;
+		int itemCount = 2000;
+		int saleCount = 3000;
+		int vendorCount = 4000;
 		
         //double[][] loanData = new double[4][100]; //0=ID, 1=loan, 2=interest rate, 3=monthly payment
 		
 		/*
 		 * ID Naming Scheme
 		 * 
+		 * customer > 0 - 999
+		 * contractor > 1000 - 1999
+		 * item >  2000 - 2999
+		 * sale > 3000 - 3999
+		 * vendor > 4000 - 4999
 		 * 
 		 */
 		
-		Customer[] customerData = new Customer[100]; //0=ID, 1=customer
-		Contractor[] contractorData = new Contractor[100]; //0=ID, 1=contractor
-		Item[] inventory = new Item[100]; //0=ID, 1=item
-		Sale[] sales = new Sale[100]; //0=ID, 1=sale
-		Vendor[] vendors = new Vendor[100]; //0=ID, 1=vendor
+		Customer[] customerData = new Customer[100];
+		Contractor[] contractorData = new Contractor[100];
+		Item[] inventory = new Item[100]; 
+		Sale[] sales = new Sale[100]; 
+		Vendor[] vendors = new Vendor[100]; 
 		
 		while(true) {
         
@@ -58,12 +67,39 @@ public class Project {
           System.out.println("2. Create Business Customer/Contractor");
           System.out.println("0. Back");
     	  int select2 = input.nextInt();
+    	  input.nextLine();
     	  
     	  switch(select2)
     	  {
     	  case 1:
+    		  int id = customerCount;
+    		  
+    		  System.out.println("First Name?");
+    		  String firstName = input.nextLine();
+    		  System.out.println("Last Name?");
+    		  String lastName = input.nextLine();
+    		  System.out.println("Address?");
+    		  String address = input.nextLine();
+    		  
+    		  boolean isValid = true;
+    		  while(isValid) {
+    			  
+    		  System.out.println("Phone Number?");
+    		  int phoneNumber = input.nextInt();
+    		  input.nextLine();
+    		  
+    		  if(phoneNumber >= 1000000000 && phoneNumber <= 9999999999L) {
+    			  
+    		  }
+    		  
+    		  }
+    		  System.out.println();
+    		  
     		  break;
     	  case 2:
+    		  int id2 = contractorCount - 1000;
+    		  
+    		  
     		  break;
     	  case 0:
     		  break;
@@ -75,6 +111,7 @@ public class Project {
           System.out.println("2. Edit Business Customer/Contractor");
           System.out.println("0. Back");
     	  int select3 = input.nextInt();
+    	  input.nextLine();
     	  
     	  switch(select3)
     	  {
