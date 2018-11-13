a5e6510381cc95139a41223960224131bc60d547
akram choukri <choukram@dukes.jmu.edu>
2018-11-12 21:25:36 -0500

    Update Sale.java

---
+++ Sale.java
@@ -0,0 +1,29 @@
+package GroupHW;
+/*
+ * @author Christopher Kang
+ * @version 11 November 2018
+ * Modified for the Group Project
+*/
+
+public class Sale {
+    public static Item itemSold;
+    public int quantity;
+    public String date;
+    public String customer;
+    
+    public Sale(Item itemSold, int quantity, String date, String customer){
+        
+        this.itemSold = itemSold;
+        this.quantity = quantity;
+        this.date = date;
+        this.customer = customer ;
+        
+    }
+    public String describeSale(){
+        System.out.printf("Item Sold:" + this.itemSold.itemName + ", " + "Price: $" + this.itemSold.getPrice() + ", " +  "Quanitity:" + 
+                quantity + ", " + "Sale Total:" + (itemSold.getPrice() * quantity));
+        
+        return ("");
+    }
+    
+}
