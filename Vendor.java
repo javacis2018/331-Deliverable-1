dedbb7b0d84f6de20368656f0733eb05492be537
javacis2018 <44952446+javacis2018@users.noreply.github.com>
2018-11-12 21:01:48 -0500

    Create Vendor.java

---
+++ Vendor.java
@@ -0,0 +1,13 @@
+public class Vendor {
+    
+    // Vendor Variables
+    String vName;
+    String vAddy;
+    String vPhone;
+    int vID;
+    int items[]; 
+    
+    public Vendor(String vName, String vAddy,
+            String vPhone, int vID)
+    
+}
