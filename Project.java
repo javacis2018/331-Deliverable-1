60f0c62879ebff462be72d8046c13610794d09fc
Chris Kang <ckang@fastmail.com>
2018-11-14 20:16:33 -0500

    Update Project.java

--- Project.java
+++ Project.java
@@ -185,14 +185,11 @@ public class Project {
     	  
     	  break;
       case 5:
-        System.out.println("Enter Sale Number");
-            if (saleCount > 2999 && saleCount < 3999){
-                input.nextInt();
-            }
-            else{
-                System.out.println("Enter a Valid Sale Number between 3000 and 3999");
-            }
+            int id4 = saleCount - 3000;
             
+            
+            
+            sales[id4] = new Sale;
     	  /*
     	   * ENTER SALE (AKRAM)
     	   */
